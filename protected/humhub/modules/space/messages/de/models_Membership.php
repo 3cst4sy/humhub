de2b05cd48d6c3f267c872b2bcc82ac0a5460f83
xTc <3cst4sy@users.noreply.github.com>
2016-03-24 20:24:41 +0100

    my installation

---
+++ protected/humhub/modules/space/messages/de/models_Membership.php
@@ -0,0 +1,12 @@
+<?php
+
+return array(
+    'Originator User ID' => '',
+    'Status' => 'Status',
+    'Request Message' => 'Anfrage-Nachricht',
+    'Last Visit' => 'Letzter Zugriff',
+    'Created At' => 'Erstellt am',
+    'Created By' => 'Erstellt von',
+    'Updated At' => 'Geändert am',
+    'Updated By' => 'Geändert um',
+);
--- protected/humhub/modules/space/messages/de/views_admin_members.php
+++ protected/humhub/modules/space/messages/de/views_admin_members.php
@@ -1,8 +1,13 @@
 <?php
-return array (
-  '<strong>Manage</strong> members' => '<strong>Mitglieder</strong> verwalten',
-  '<strong>Manage</strong> permissions' => '<strong>Mitglieder</strong> Berechtigungen',
-  '<strong>Pending</strong> approvals' => '<strong>Ausstehende</strong> Freigaben',
-  '<strong>Pending</strong> invitations' => '<strong>Ausstehende</strong> Einladungen',
-  'never' => 'nie',
+
+return array(
+    '<strong>Manage</strong> members' => '<strong>Mitglieder</strong> verwalten',
+    '<strong>Manage</strong> permissions' => '<strong>Mitglieder</strong> Berechtigungen',
+    '<strong>Pending</strong> approvals' => '<strong>Ausstehende</strong> Freigaben',
+    '<strong>Pending</strong> invitations' => '<strong>Ausstehende</strong> Einladungen',
+    'never' => 'nie',
+    '<strong>Current Group:</strong>' => '',
+    'Group' => 'Gruppe',
+    'Actions' => '',
+    'Remove' => 'Entfernen',
 );
